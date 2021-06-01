078aaa21c33a5db9a294f5aa9f6a2e33f6a2581b
Thais Hoshii <53278565+Thais-Hoshii@user.noreply.github.com>
2021-06-01 16:13:40 -0300

    Corrected paths and database config
    I corrected some of the paths in package.json and routes.js because I forgot to add "app/". Also added some config data to directly refer to the database because it was throwing an error: The server does not support SSL connections. These were commented for now until further investigation.

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path')
 
-const UserController = require('./controllers/UserController');
+const UserController = require('./app/controllers/UserController');
 
 const routes = express.Router();
 
