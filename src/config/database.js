824be050e15114ff3f6d0e35cf415cae99f1a596
Thais Hoshii <53278565+Thais-Hoshii@user.noreply.github.com>
2021-06-01 16:43:06 -0300

    Fix the connection error
    I added the direct values for the Heroku database into the .env file as sepearate variables. Also install dotenv and added a require('dotenv/config') inside config/database, before the export.

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,5 @@
+require('dotenv/config');
+
 module.exports = {
   dialect: 'postgres',
   dialectOptions: {
@@ -10,10 +12,6 @@ module.exports = {
   username: process.env.USER, 
   password:  process.env.PASSWORD, 
   database: process.env.DATABASE,
-  // host: 'ec2-23-21-229-200.compute-1.amazonaws.com', 
-  // username: 'bdlwweevrljiqx', 
-  // password: '1bbdac1e1dcdd4efa746bd6114d672d328a46ce17ae172209ab309569365ad93', 
-  // database: 'ddvkv5irs3dhs1',
   define: {
       timestamps: true,  //
   }
