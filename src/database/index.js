679b17152e4d613f2d522f40e37b6a0eccf4dede
Thais Hoshii <53278565+Thais-Hoshii@user.noreply.github.com>
2021-05-31 22:52:59 -0300

    Added latest changes from main branch

--- src/database/index.js
+++ src/database/index.js
@@ -1,41 +1,20 @@
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config.json')[env];
-const db = {};
+const dbConfig = require('../config/database');
 
-//const User = require('./User') //acrescentei por causa do model
+const User = require('../app/models/User');
 
-let sequelize; //connection
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+const models = [User];
+
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(dbConfig);
+    models.map( (model) => { model.init(this.connection) })
+  }
 }
 
-//User.init(sequelize); //inicializei o model
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+module.exports = new Database();
---
+++ src/routes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path')
+
+const UserController = require('./controllers/UserController');
+
+const routes = express.Router();
+
+  routes.get('/', (req, res) => {                         //sendfile('./pasta/index.html') foi descontinuado, 
+    res.sendFile(path.join(__dirname, 'index.html'))        //deve-se utilizar sendFile() com o path                                                    
+  })
+  
+  routes.get('/users', UserController.index);
+  routes.post('/users', UserController.store);
+  
+  
+  routes.get('/clients',(req,res) => {
+      res.send({'message':'clientes'});
+  })
+  
+  routes.get('/addresses',(req,res) => {
+      res.send({'message':'Endereços'});
+  })
+  
+  routes.get('/specialists',(req,res) => {
+      res.send({'message':'Especialistas'});
+  })
+  
+  routes.get('/schedules',(req,res) => {
+      res.send({'message':'Atendimentos ou agendamentos'});
+  })
+
+  module.exports = routes;
