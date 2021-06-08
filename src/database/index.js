55e6d661f9c1db986c12955061bf5318e03fcb4f
Thais Hoshii <53278565+Thais-Hoshii@users.noreply.github.com>
2021-06-08 15:40:26 -0300

    Reference the models in the index.js
    Added references to the models used in the index.js

--- src/database/index.js
+++ src/database/index.js
@@ -2,8 +2,10 @@ const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
 // const User = require('../app/models/User');
+const Endereco = require('../app/models/Endereco');
+const Paciente = require('../app/models/Paciente');
 
-const models = [];
+const models = [Endereco, Paciente];
 
 
 class Database {
