129dacf1e25ad3b29edd009f7e01568c34226ae7
Thais Hoshii <53278565+Thais-Hoshii@users.noreply.github.com>
2021-06-14 22:32:04 -0300

    Prepare the project for tests
    Added a new package, jest, to automatically execute unit tests from a single command. Added test-specific scripts and a .env.test file to add a new environment variable that enables a test database.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
 
-
+require("dotenv").config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+});
 
 class App {
   constructor() {
--- src/config/database.js
+++ src/config/database.js
@@ -1,20 +1,45 @@
-require('dotenv/config');
+require("dotenv").config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+});
 
-module.exports = {
-  dialect: 'postgres',
-  dialectOptions: {              //necessário para rodar no heroku
-      ssl: {                        //necessário para rodar no heroku
-        require: true,              //necessário para rodar no heroku
-        rejectUnauthorized: false   //necessário para rodar no heroku
-      }                             //necessário para rodar no heroku
-  },                                //necessário para rodar no heroku*/
-  host: process.env.HOST, 
-  username: process.env.USER, 
-  password:  process.env.PASSWORD, 
-  database: process.env.DATABASE,
-  define: {
-      timestamps: true,  //
-      underscored: true,  // snake_case
-      underscoredAll: true
+let db = {};
+
+if (process.env.NODE_ENV === "test") {
+  db = {
+    dialect: process.env.DB_DIALECT,
+    storage: './__tests__/database.sqlite',
+    retry: {
+      max: 10,
+      match: [
+        'SQLITE_BUSY: database is locked'
+      ]
+    },
+    define: {
+        timestamps: true,  //
+        underscored: true,  // snake_case
+        underscoredAll: true
+    }
   }
 }
+else {
+  db = {
+    dialect: 'postgres',
+    dialectOptions: {              //necessário para rodar no heroku
+        ssl: {                        //necessário para rodar no heroku
+          require: true,              //necessário para rodar no heroku
+          rejectUnauthorized: false   //necessário para rodar no heroku
+        }                             //necessário para rodar no heroku
+    },                                //necessário para rodar no heroku*/
+    host: process.env.HOST, 
+    username: process.env.USER, 
+    password:  process.env.PASSWORD, 
+    database: process.env.DATABASE,
+    define: {
+        timestamps: true,  //
+        underscored: true,  // snake_case
+        underscoredAll: true
+    }
+  }
+}
+
+module.exports = db;
