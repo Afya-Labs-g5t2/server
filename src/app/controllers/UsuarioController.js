38ae441d8e81fe33501a8832f8e82abce72a8b78
Renato Lobo Rodrigues Júnior <renatolobojr@users.noreply.github.com>
2021-06-21 10:56:24 -0300

    Revert "Population"

--- src/app/controllers/UsuarioController.js
+++ src/app/controllers/UsuarioController.js
@@ -3,28 +3,13 @@ const Usuario = require('../models/Usuario');
 class UsuarioController {
   async index(req, res) {
     try {
-      const temp = await Usuario.findAll({attributes:['id','nome', 'createdAt', "updatedAt"]});
+      const temp = await Usuario.findAll();
 
       return res.json(temp);
     } catch (err) {
       return res.status(400).json({ error: err.message });
     }
   }
-
-  async show(req, res) {
-    try {
-      
-      if (req.params.id<=0) return res.status(418).json({ error: "São aceitos somente valores de Id maiores do que zero" });
-
-      const temp = await Usuario.findByPk(req.params.id,{attributes:['id','login','nome', 'createdAt', "updatedAt"]});
-
-      if (!temp) return res.status(404).json({ error: "Não existe nenhum Usuario com esse id" });      
-
-      return res.json(temp);
-    } catch (err) {
-      return res.status(400).json({ error: err.message });
-    }
-  }//  */
 
   async store(req, res) {
     try {
@@ -32,30 +17,6 @@ class UsuarioController {
       let {login, nome, senha, createdAt, updatedAt } = temp;
       senha = "****************"
       return res.json({login, nome, senha, createdAt, updatedAt });
-    } catch (err) {
-      return res.status(400).json({ error: err.message });
-    }
-  }
-
-  async update(req, res) {
-    try {
-      const temp = await Usuario.findByPk(req.params.id);
-
-      await temp.update(req.body);
-
-      return res.json({ temp });
-    } catch (err) {
-      return res.status(400).json({ error: err.message });
-    }
-  }
-
-  async destroy(req, res) {
-    try {
-      const temp = await Usuario.findByPk(req.params.id);
-
-      await temp.destroy();
-
-      return res.json();
     } catch (err) {
       return res.status(400).json({ error: err.message });
     }
